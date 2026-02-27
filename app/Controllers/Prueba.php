66523cfcf1e7db79187c4feec05a147390bd7f14
cesar <cesar@CESARB>
2026-02-26 22:14:34 -0600

    Primer committ

--- app/Controllers/Prueba.php
+++ app/Controllers/Prueba.php
@@ -6,13 +6,28 @@ use CodeIgniter\Controller;
 
 class Prueba extends Controller
 {
-    public function insertar()
+   
+public function insertar()
 {
     try {
         $db = \Config\Database::connect();
-        echo "✅ Conectado correctamente";
+
+        $data = [
+            'nombre' => 'Prueba Railway'
+        ];
+
+        $insert = $db->table('prueba')->insert($data);
+
+        if ($insert) {
+            echo "✅ Insertado correctamente";
+        } else {
+            echo "❌ No se insertó";
+            print_r($db->error());
+        }
+
     } catch (\Throwable $e) {
         echo "❌ Error: " . $e->getMessage();
     }
 }
+
 }
